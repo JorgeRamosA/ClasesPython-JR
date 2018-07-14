0044fe03b653682b0ccd288d9bbd5bd67d011270
Jorge Ramos Aillón <joborall@yahoo.com>
2018-07-13 21:52:54 -0500

    cargo mi primer programa en python

---
+++ ejemplo1.py
@@ -0,0 +1,12 @@
+//declaro una variable y solicito que ingrese un número
+numero1 = int(input("Ingrese un número: "))
+//declaro la variable numero2 y solicito el ingreso de un número
+numero2 = int(input("Ingrese otro numero}: "))
+//imprimo en pantalla la operación suma
+print(f"La suma entre ellos es {numero1 + numero2}.")
+//imprimo en pantalla la operación resta
+print(f"La diferencia entre ellos es {numero1 - numero2}.")
+//imprimo en pantalla la operación multiplicación
+print(f"La multipliación entre ellos es {numero1 * numero2}.")
+//imprimo en pantalla la operación división
+print(f"La dvición entre ellos es {numero1 / numero2}.")
